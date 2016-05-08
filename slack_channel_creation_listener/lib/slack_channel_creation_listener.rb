bf70d56d434e257141e3f2e9b79fde62394234e2
Adam Price <adamhowardprice@gmail.com>
2016-05-08 23:06:24 +0000

    Use a different Slack gem, fix Google Sheets logic

--- slack_channel_creation_listener/lib/slack_channel_creation_listener.rb
+++ slack_channel_creation_listener/lib/slack_channel_creation_listener.rb
@@ -1,5 +1,5 @@
 require 'dotenv'
-require 'slack'
+require 'slack-ruby-client'
 require 'google_drive'
 
 # load environment
@@ -7,13 +7,15 @@ Dotenv.load
 
 Slack.configure do |config|
 	config.token = ENV['SLACK_API_TOKEN']
+	config.logger = Logger.new(STDOUT)
+	config.logger.level = Logger::INFO
+	fail "Missing ENV['SLACK_API_TOKEN']!" unless config.token
 end
 
-gd_session = GoogleDrive.saved_session("../google_drive_config.json")
-
 # setup clients
-client = Slack::Client.new
+client = Slack::Web::Client.new(user_agent: 'Slack Ruby Client/1.0')
 rt_client = Slack::RealTime::Client.new
+gd_session = GoogleDrive.saved_session("../google_drive_config.json")
 
 rt_client.on :channel_created do
 	# whenever a channel is created,
@@ -28,12 +30,18 @@ rt_client.on :channel_created do
 
 	# post to google sheets
 	puts 'Updating Google Sheets'
-	ws = gd_session.spreadsheet_by_key(ENV['GOOGLE_DRIVE_SHEET_ID']).worksheets[0]
-	channels.each_with_index do |c, idx|
-		ws[idx, 1] = c["name"]
-		ws[idx, 2] = c["id"]
+	begin
+		ws = gd_session.spreadsheet_by_key(ENV['GOOGLE_DRIVE_SHEET_ID']).worksheets[0]
+		puts "Worksheet Title: #{ws.title}"
+		channels.each_with_index do |c, idx|
+			# Google Sheets are indexed [row, col] and start at top-left [1, 1]
+			ws[idx + 1, 1] = c["name"]
+			ws[idx + 1, 2] = c["id"]
+		end
+		ws.save
+	rescue Exception => e
+		$stderr.print "Failed to update Google Sheets\n#{e.inspect}"
 	end
-	ws.save
 
 end
 
