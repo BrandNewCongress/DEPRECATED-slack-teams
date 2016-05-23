ce882bbd88d2f22bc19f75247be7798c74bc0a41
Adam Price <adamprice@spotify.com>
2016-05-23 19:41:41 -0400

    Tabs to spaces

--- tour_slackbot/lib/utils/configure_clients.rb
+++ tour_slackbot/lib/utils/configure_clients.rb
@@ -5,24 +5,24 @@ require 'form_copy_apps_script_executor'
 
 
 module ConfigureClients
-	def configure_slack
-		Dotenv.load
-		Slack.configure do |config|
-			config.token = ENV['SLACK_API_TOKEN']
-			fail "Missing ENV['SLACK_API_TOKEN']!" unless config.token
-		end
+  def configure_slack
+    Dotenv.load
+    Slack.configure do |config|
+      config.token = ENV['SLACK_API_TOKEN']
+      fail "Missing ENV['SLACK_API_TOKEN']!" unless config.token
+    end
 
-		client = Slack::Web::Client.new(user_agent: 'Slack Ruby Client/1.0')
-		client
-	end
+    client = Slack::Web::Client.new(user_agent: 'Slack Ruby Client/1.0')
+    client
+  end
 
-	def configure_google_drive
-		session = GoogleDrive.saved_session("../../../../google_drive_config.json")
-		session
-	end
+  def configure_google_drive
+    session = GoogleDrive.saved_session("../../../../google_drive_config.json")
+    session
+  end
 
-	def configure_apps_script_executor
-		executor = FormCopyAppsScriptExecutor.new
-		executor
-	end
+  def configure_apps_script_executor
+    executor = FormCopyAppsScriptExecutor.new
+    executor
+  end
 end
