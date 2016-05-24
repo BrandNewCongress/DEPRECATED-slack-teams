ccef0a54456b09463be96cb4d5de78705597588a
Adam Price <adamprice@spotify.com>
2016-05-24 01:43:19 -0400

    Working on commands

--- tour_slackbot/lib/tour_slackbot.rb
+++ tour_slackbot/lib/tour_slackbot.rb
@@ -1,2 +1,4 @@
 require 'tour_slackbot/server'
 require 'tour_slackbot/slackbot_app'
+require 'tour_slackbot/commands/events'
+require 'tour_slackbot/commands/help'
--- tour_slackbot/lib/tour_slackbot/slackbot_app.rb
+++ tour_slackbot/lib/tour_slackbot/slackbot_app.rb
@@ -1,4 +1,15 @@
 module TourSlackBot
   class App < SlackRubyBot::Bot
+
+    # help do
+    #   title 'BNC Bot'
+    #   desc "I'm here to help you get started with Brand New Congress!"
+
+    #   command 'events' do
+    #     desc "Tells you all the events coming up on the BNC tour. Join the Slack group for the city you're volunteering in!"
+    #   end
+    # end
+
+    
   end
 end
